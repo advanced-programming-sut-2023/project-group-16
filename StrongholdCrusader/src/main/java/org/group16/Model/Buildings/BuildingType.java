3183368d92c6697ede7beb3c694e6cb41c62cab1
yashar paymai <yasharp1383@gmail.com>
2023-05-04 21:15:58 +0330

    building names in string form added

--- StrongholdCrusader/src/main/java/org/group16/Model/Buildings/BuildingType.java
+++ StrongholdCrusader/src/main/java/org/group16/Model/Buildings/BuildingType.java
@@ -2,93 +2,97 @@ package org.group16.Model.Buildings;
 
 public enum BuildingType {
     //economy
-    SMALL_STONE_GATEHOUSE,
+    SMALL_STONE_GATEHOUSE("small stone gatehouse"),
     //economy
-    LARGE_STONE_GATEHOUSE,
+    LARGE_STONE_GATEHOUSE("large stone gatehouse"),
     //war
-    DRAWBRIDGE,
+    DRAWBRIDGE("drawbridge"),
     //war
-    LOOKOUT_TOWER,
+    LOOKOUT_TOWER("lookout tower"),
     //war
-    PERIMETER_TOWER,
+    PERIMETER_TOWER("perimeter tower"),
     //war
-    DEFENCE_TURRET,
+    DEFENCE_TURRET("defence turret"),
     //war
-    SQUARE_TOWER,
+    SQUARE_TOWER("square tower"),
     //war
-    ROUND_TOWER,
+    ROUND_TOWER("round tower"),
     //economy
-    ARMOURY,
+    ARMOURY("armoury"),
     //economy
-    BARRACKS,
+    BARRACKS("barracks"),
     //economy
-    MERCENARY_POST,
+    MERCENARY_POST("mercenary post"),
     //economy
-    ENGINEER_GUILD,
+    ENGINEER_GUILD("engineer guid"),
     //war
-    KILLING_PIT,
+    KILLING_PIT("killing pit"),
     //economy
-    INN,
+    INN("inn"),
     //economy
-    MILL,
+    MILL("mill"),
     //economy
-    IRON_MINE,
+    IRON_MINE("iron mine"),
     //economy
-    MARKET,
+    MARKET("market"),
     //economy
-    OX_TETHER,
+    OX_TETHER("ox tether"),
     //economy
-    PITCH_RIG,
+    PITCH_RIG("pitch rig"),
     //economy
-    QUARRY,
+    QUARRY("quarry"),
     //economy
-    STOCKPILE,
+    STOCKPILE("stockpile"),
     //economy
-    WOOD_CUTTER,
+    WOOD_CUTTER("wood cutter"),
     //economy
-    HOVEL,
+    HOVEL("hove"),
     //economy
-    CHURCH,
+    CHURCH("church"),
     //economy
-    CATHEDRAL,
+    CATHEDRAL("cathedral"),
     //economy
-    ARMOURER,
+    ARMOURER("armourer"),
     //economy
-    BLACKSMITH,
+    BLACKSMITH("blacksmith"),
     //economy
-    FLETCHER,
+    FLETCHER("fletcher"),
     //economy
-    POLE_TURNER,
+    POLE_TURNER("pole turner"),
     //economy
-    OIL_SMELTER,
+    OIL_SMELTER("oil smelter"),
     //TODO : ????
     //war ?
-    PITCH_DITCH,
+    PITCH_DITCH("pitch dig"),
     //TODO : ????
     //war?
-    CAGED_WAR_DOGS,
+    CAGED_WAR_DOGS("caged war dogs"),
     //TODO : ????
     //war?
-    SIEGE_TENT,
+    SIEGE_TENT("siege tent"),
     //economy
-    STABLE,
+    STABLE("stable"),
     //economy
-    APPLE_ORCHARD,
+    APPLE_ORCHARD("apple orchard"),
     //economy
-    DIARY_FARMER,
+    DIARY_FARMER("diary farm"),
     //economy
-    HOPS_FARMER,
+    HOPS_FARMER("hops farmer"),
     //economy
-    HUNTER_POST,
+    HUNTER_POST("hunter post"),
     //economy
-    WHEAT_FARMER,
+    WHEAT_FARMER("wheat farmer"),
     //economy
-    BAKERY,
+    BAKERY("bakery"),
     //economy
-    BREWER,
+    BREWER("brewer"),
     //economy
-    GRANARY,
-    TOWN_BUILDING ,
-    UNEMPLOYED_PLACE
+    GRANARY("granary"),
+    TOWN_BUILDING("town building") ,
+    UNEMPLOYED_PLACE("unemployed place")
     ;
+    private final String strName ;
+    private BuildingType(String strName){
+        this.strName = strName ;
+    }
 }
