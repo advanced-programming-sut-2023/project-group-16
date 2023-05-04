06a759a199332235f43c4640097240be7230cf33
yashar paymai <yasharp1383@gmail.com>
2023-05-04 22:00:49 +0330

    added property okToBuildIn in CellType

--- StrongholdCrusader/src/main/java/org/group16/Model/CellType.java
+++ StrongholdCrusader/src/main/java/org/group16/Model/CellType.java
@@ -1,28 +1,30 @@
 package org.group16.Model;
 
 public enum CellType {
-    NORMAL(1),
-    SMALL_STONE(1),
-    ROCK(2),
-    STONE(Integer.MAX_VALUE),
-    IRON(1),
-    GRASS(1),
-    LOW_DENSITY_GRASSLAND(1),
-    HIGH_DENSITY_GRASSLAND(1),
-    OIL(1),
-    WET_PLAIN(Integer.MAX_VALUE),
-    SHALLOW_WATER(2),
-    BEACH(1),
-    RIVER(Integer.MAX_VALUE),
-    POND(Integer.MAX_VALUE),
-    SEA(Integer.MAX_VALUE),
+    NORMAL(1 , true),
+    SMALL_STONE(1 , true),
+    ROCK(2 , false),
+    STONE(Integer.MAX_VALUE , false),
+    IRON(1 , true),
+    GRASS(1 , true),
+    LOW_DENSITY_GRASSLAND(1 , true),
+    HIGH_DENSITY_GRASSLAND(1 , true),
+    OIL(1 , true),
+    WET_PLAIN(Integer.MAX_VALUE , false),
+    SHALLOW_WATER(2 , false),
+    BEACH(1 , true),
+    RIVER(Integer.MAX_VALUE , false),
+    POND(Integer.MAX_VALUE , false),
+    SEA(Integer.MAX_VALUE , false),
     ;
     //TODO: Fill this enum
 
     private final double traverseCost;
+    private final boolean okToBuildIn ;
 
-    private CellType(double traverseCost) {
+    private CellType(double traverseCost , boolean okToBuildIn) {
         this.traverseCost = traverseCost;
+        this.okToBuildIn = okToBuildIn ;
     }
 
     public double getTraverseCost() {
