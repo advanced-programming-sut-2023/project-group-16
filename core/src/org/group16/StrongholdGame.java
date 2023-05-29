9e5dadb8a70d8b3a35b5b372266f593938315664
MahdiGMK <mbahramiand@gmail.com>
2023-05-29 23:01:29 +0330

    badlogic

--- core/src/org/group16/StrongholdGame.java
+++ core/src/org/group16/StrongholdGame.java
@@ -1,11 +1,12 @@
-package com.mygdx.game;
+package org.group16;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class StrongholdGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
