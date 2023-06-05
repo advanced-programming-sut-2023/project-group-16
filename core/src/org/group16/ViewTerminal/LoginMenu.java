42d1414b65ad05f20e3707f4590dd3a8073b41bf
yashar paymai <yasharp1383@gmail.com>
2023-06-05 13:27:59 +0330

    registerScreen Completed

--- core/src/org/group16/ViewTerminal/LoginMenu.java
+++ core/src/org/group16/ViewTerminal/LoginMenu.java
@@ -173,25 +173,11 @@ public class LoginMenu {
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             TreeMap<String, ArrayList<String>> map;
-            if ((map = CommandHandler.matches(Command.CREATE_USER, input)) != null) createUser(map);
-            else if ((map = CommandHandler.matches(Command.CREATE_USER_WITH_RANDOM_PASSWORD, input)) != null)
-                createUser(map);
-            else if ((map = CommandHandler.matches(Command.LOGIN, input)) != null) loginUser(map);
+            if ((map = CommandHandler.matches(Command.LOGIN, input)) != null) loginUser(map);
             else if ((map = CommandHandler.matches(Command.FORGOT_PASSWORD, input)) != null) forgotPassword(map);
             else if (CommandHandler.matches(Command.EXIT, input) != null) break;
             else System.out.println("invalid command");
         }
-    }
-
-    private void createUser(TreeMap<String, ArrayList<String>> map) {
-        String username = map.get("u").get(0);
-        String password = map.containsKey("p") ? map.get("p").get(0) : "random";
-        String passwordConfirmation = map.containsKey("p") ? map.get("p").get(1) : "random";
-        String email = map.get("e").get(0);
-        String nickname = map.get("n").get(0);
-        String slogan = map.containsKey("s") ? map.get("s").get(0) : "";
-        System.out.println(LoginMenuController.createUser(username, password, passwordConfirmation,
-                email, nickname, slogan));
     }
 
     private void loginUser(TreeMap<String, ArrayList<String>> map) {
