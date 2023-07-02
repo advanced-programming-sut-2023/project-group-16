a385fb42186484b99bf4e412ae091bda527e30b2
MahdiGMK <mbahramiand@gmail.com>
2023-07-02 05:26:37 +0330

    networking...

--- core/src/org/group16/Networking/LobbySocket.java
+++ core/src/org/group16/Networking/LobbySocket.java
@@ -1,12 +1,17 @@
 package org.group16.Networking;
 
+import org.group16.Model.Map;
+import org.group16.Model.User;
+import org.group16.Model.UserList;
 import org.group16.StrongholdGame;
 
+import java.awt.desktop.UserSessionEvent;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class LobbySocket {
     public static Socket socket;
@@ -19,5 +24,54 @@ public class LobbySocket {
         cmdStream = new DataOutputStream(socket.getOutputStream());
         inputStream = new ObjectInputStream(socket.getInputStream());
         outputStream = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public static User getUser(String username) throws IOException, ClassNotFoundException {
+        cmdStream.writeUTF(
+                String.format("get user -u \"%s\"", username));
+        return (User) inputStream.readObject();
+    }
+
+    public static ArrayList<User> getAllUsers() throws IOException, ClassNotFoundException {
+        cmdStream.writeUTF("get all users");
+        return ((UserList) inputStream.readObject()).users;
+    }
+
+    public static String login(String username, String password) throws IOException {
+        cmdStream.writeUTF(
+                String.format("get user -u \"%s\" -p \"%s\"", username, password));
+        return inputStream.readUTF();
+    }
+
+    public static String register(String username, String password, String nick, String email, String slogan, String question, String answer) {
+        
+    }
+
+    public static String forgotPassword(String username, String answer, String newPassword) {
+
+    }
+
+    public static String logout() {
+
+    }
+
+    public static String changeProfile(String username, String password, String nick, String email, String slogan) {
+
+    }
+
+    public static int displayScore() {
+
+    }
+
+    public static int displayRank() {
+
+    }
+
+    public static void uploadMap(Map map) {
+
+    }
+
+    public static Map downloadMap(String mapname) {
+
     }
 }
--- desktop/src/org/group16/DesktopLauncher.java
+++ desktop/src/org/group16/DesktopLauncher.java
@@ -4,10 +4,23 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import games.spooky.gdx.nativefilechooser.NativeFileChooserConfiguration;
 import games.spooky.gdx.nativefilechooser.desktop.DesktopFileChooser;
+import org.group16.Networking.GameSocket;
+import org.group16.Networking.LobbySocket;
+import org.group16.Server.Server;
+
+import java.io.IOException;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
     public static void main(String[] arg) {
+        try {
+            LobbySocket.createSocket(Server.host, Server.lobbyPort);
+        } catch (IOException e) {
+            System.out.println("Didnt Connect To Server");
+            return;
+        }
+        GameSocket.initSocket(Server.host, Server.lobbyPort);
+
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
         config.useVsync(true);
@@ -15,7 +28,6 @@ public class DesktopLauncher {
         config.setWindowedMode(1600, 1280);
         StrongholdGame.fileChooser = new DesktopFileChooser();
         StrongholdGame.fileChooserConfiguration = new NativeFileChooserConfiguration();
-//        LobbySocket.createSocket("localhost", Server.lobbyPort);
 
         config.setWindowIcon("icons/game_icon.png");
         new Lwjgl3Application(new StrongholdGame(), config);
